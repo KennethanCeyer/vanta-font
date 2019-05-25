a10c09af4d6f5cb6076e5c65bdc4bcfd56cdc8f5
Kenneth Ceyer <kennethan@nhpcw.com>
2019-05-25 15:30:42 +0900

    WIP: add test env of gtest

--- tests/render_test.cpp
+++ tests/render_test.cpp
@@ -1,4 +1,4 @@
-/*
+;/*
  * Copyright (c) 2019 PIGNOSE. All rights reserved.
  *
  * This source is govered under the MIT license.
@@ -6,11 +6,16 @@
  *
  */
 
-#include <stdio.h>
+#include <iostream>
 #include <gtest/gtest.h>
 #include "../src/font.c"
 
 TEST(font, render) {
     vt_load_font("./data/arial.ttf");
-    printf("A");
+    std::cout << "test";
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
